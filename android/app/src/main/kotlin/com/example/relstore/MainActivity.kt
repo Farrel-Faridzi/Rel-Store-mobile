ff5ffd176056bcdc648022130d624d5238db81f0
Farrel <farrelacc2@gmail.com>
2025-10-29 15:30:24 +0700

    Tugas 7

---
+++ android/app/src/main/kotlin/com/example/relstore/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.relstore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/menu.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+    MyHomePage({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+        title: const Text(
+          'Rel Store',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        ),
+
+        body: Padding(
+        padding: const EdgeInsets.all(16.0),
+          child: Column(
+                // Menyusun teks dan grid item secara vertikal.
+            children: [
+              // Menampilkan teks sambutan dengan gaya tebal dan ukuran 18.
+              const Padding(
+                padding: EdgeInsets.only(top: 16.0),
+                child: Text(
+                  'Selamat datang di Rel Store',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+
+              GridView.count(
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                // Agar grid menyesuaikan tinggi kontennya.
+                shrinkWrap: true,
+
+                children: items.map((ItemHomepage item) {
+                  return ItemCard(item);
+                }).toList(),
+              )
+            ]
+          )
+        )
+      );
+    }
+
+    final List<ItemHomepage> items = [
+    ItemHomepage("All Products", Icons.storefront),
+    ItemHomepage("My Products", Icons.inventory),
+    ItemHomepage("Create Product", Icons.add_circle),
+  ];
+}
+
+
+class ItemHomepage {
+  final String name;
+  final IconData icon;
+
+  ItemHomepage(this.name, this.icon);
+}
+
+class ItemCard extends StatelessWidget {
+  // Menampilkan kartu dengan ikon dan nama.
+
+  final ItemHomepage item; 
+
+  const ItemCard(this.item, {super.key}); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      // Menentukan warna latar belakang dari tema aplikasi.
+      color: Theme.of(context).colorScheme.secondary,
+      // Membuat sudut kartu melengkung.
+      borderRadius: BorderRadius.circular(12),
+
+      child: InkWell(
+        // Aksi ketika kartu ditekan.
+        onTap: () {
+          // Menampilkan pesan SnackBar saat kartu ditekan.
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
+            );
+        },
+        // Container untuk menyimpan Icon dan Text
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              // Menyusun ikon dan teks di tengah kartu.
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
